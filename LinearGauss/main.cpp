1f9034b3e44bebce6818033bebaa78a2b80af84b
AndrewZh <hotsmileband@gmail.com>
2015-04-11 16:58:04 +0300

    Add input generation and result check

--- LinearGauss/main.cpp
+++ LinearGauss/main.cpp
@@ -1,29 +1,25 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
-
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 #define N 3
 
+void generateInput(double**& matrix, double*& answer);
+void init(double**& matrix);
+void clear(double**& matrix);
+
 int main()
 {
-	double a[N][N+1] = {0};
-	double x[N] = {0};
-
-	cout << setprecision(2);
-	ifstream in;
-	in.open("input.txt");
-
-	for (int i=0; i<N; i++) 
-	{
-		for (int j=0; j<N+1; j++) 
-		{
-			in >> a[i][j];
-			cout << a[i][j] << " ";
-		}
-	}
-
+	double** a;//[N][N+1];
+        double* answer = new double[N];
+	double* x = new double[N];
+        
+        generateInput(a, answer);
+	
+        cout << setprecision(2);
 	for (int i=0; i<N; i++)
 	{
 		if (a[i][i] == 0)
@@ -71,5 +67,52 @@ int main()
 	for (int i=0; i<N; i++)
 		cout << "x[" << i << "] = " << x[i] << "\n";
 	cout << endl << endl;
+        
+        bool flag = true;
+        for (int i = 0; i < N; ++i) {
+            if (x[i] != answer[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cout << "ERROR\n"; 
+        } else {
+            cout << "OK\n";
+        }
         cin.ignore().get();
+}
+
+void generateInput(double**& matrix, double*& answer) 
+{
+    init(matrix);
+    srand(time(0));    
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix[i][j] = rand() % 100;
+            cout << matrix[i][j] << " ";
+        }        
+        answer[i] = rand()%100;        
+        cout << endl;
+    }
+    for (int i = 0; i < N; ++i) {
+        matrix[i][N] = 0;
+        for (int j = 0; j < N; ++j) {
+            matrix[i][N] += matrix[i][j] * answer[j];
+        }
+    }
+}
+void init(double**& matrix)
+{
+    matrix = new double*[N];
+    for (int i = 0; i < N; ++i) {
+        matrix[i] = new double[N+1];
+    }
+}
+void clear(double**& matrix)
+{
+    for (int i = 0; i < N; ++i) {
+        delete [] matrix[i];
+    }
+    delete [] matrix;
 }
